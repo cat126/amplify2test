b7a5ac3e706cbda932a5e98502edfc64ea739d3b
Bruce Smith <cat1265@gmail.com>
2024-10-23 00:04:51 -0500

    fixed " @auth directive with 'apiKey' provider found, but the project has no API Key authentication provider configured."

--- amplify2test/amplify/data/resource.ts
+++ amplify2test/amplify/data/resource.ts
@@ -11,7 +11,7 @@ const schema = a.schema({
     .model({
       content: a.string(),
     })
-        .authorization((allow) => [allow.publicApiKey()]),
+        .authorization((allow) => [allow.authenticated()]),
 
     PlayerEvent: a.model({
         itemID: a.string().required(),
